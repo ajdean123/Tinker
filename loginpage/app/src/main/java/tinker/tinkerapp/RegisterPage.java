1fc54e29c91d32f707db79eb65b450d305f242d7
Ajwad Dean <s3436888@student.rmit.edu.au>
2018-10-02 23:21:41 +1000

    added ui for registration page

--- loginpage/app/src/main/java/tinker/tinkerapp/RegisterPage.java
+++ loginpage/app/src/main/java/tinker/tinkerapp/RegisterPage.java
@@ -2,6 +2,7 @@ package tinker.tinkerapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class RegisterPage extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class RegisterPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_page);
+
+        final EditText RegName = findViewById(R.id.RegName);
+        final EditText RegEmail = findViewById(R.id.RegEmail);
+        final EditText RegDOB = findViewById(R.id.RegDOB);
+        final EditText RegPassword = findViewById(R.id.RegPassword);
+
     }
 }
---
+++ loginpage/app/src/main/java/tinker/tinkerapp/userAreaActivity.java
@@ -0,0 +1,13 @@
+package tinker.tinkerapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class userAreaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_area);
+    }
+}
